e02da6c800bf4fe78002567f379c33e472fea997
Angel Yang <101146704+angely119@users.noreply.github.com>
2022-06-09 00:01:51 -0400

    Completed Backend Routes

--- server/api/rentals.js
+++ server/api/rentals.js
@@ -27,32 +27,4 @@ router.get('/:id', async (req, res, next) => {
   }
 });
 
-// POST /api/rentals/:rentalId/bookings - Creates a booking for a rental
-router.post('/:rentalId/bookings', async (req, res, next) => {
-  try {
-    const newBooking = await Booking.create(req.body);
-    // req.body includes rentalId
-    // can set guest in req.body
-    res.send(await newBooking.reload({
-      include: ['guest', Rental]
-    })); // sends back the new Booking including guest and rental
-  } catch (error) {
-    next(error);
-  }
-});
-
-// POST /api//rentals/:rental_id/reviews - Creates a review for a rental
-router.post('/:rentalId/reviews', async (req, res, next) => {
-  try {
-    const newReview = await Review.create(req.body);
-    // req.body includes rentalId
-    // can also set user in req.body
-    res.send(await newReview.reload({
-      include: [User, Rental]
-    })); // sends back the new Booking including guest and rental
-  } catch (error) {
-    next(error);
-  }
-});
-
 module.exports = router;
