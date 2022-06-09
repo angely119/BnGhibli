4a9afbe87f0ebe03ca4c4fa4713e349b4a2a0cbf
Angel Yang <101146704+angely119@users.noreply.github.com>
2022-06-09 15:29:05 -0400

    Styled Footer Component

--- client/components/Footer.js
+++ client/components/Footer.js
@@ -1,19 +1,45 @@
 import React from "react";
 import styled from "styled-components";
+import { StyledLink, LinkContainer } from "./NavBar";
 
 const StyledFooter = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
   position: absolute;
   width:100%;
-  left: 20px;
-  height: 20px;
-  padding: 10px;
+  height: 40px;
   border-top: solid grey 1px;
+`;
+
+const StyledSpan = styled.span`
+  display: flex;
+  align-items: flex-end;
+  height: 30px;
+  color: black;
+  font-size: 15px;
+  margin-right: 20px;
+`;
+
+const FooterLinkContainer = styled(LinkContainer)`
+  margin-left: 40px;
+  height: 30px;
+`;
+
+const FooterStyledLink = styled(StyledLink)`
+  align-items: flex-end;
+  font-size: 15px;
 `;
 
 const Footer = () => {
   return (
     <StyledFooter>
-      <span>FOOTER</span>
+      <FooterLinkContainer>
+        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSzh1ST7YGDBCT54ObrjXk_ORrRGwVL366Jb48Zz6r66YP2rcl2GNFRGQPg3hUvpOXHBEY&usqp=CAU' width="50" height="50"/>
+        <FooterStyledLink to="/home">Ghibli'nB</FooterStyledLink>
+      </FooterLinkContainer>
+      <StyledSpan>Created by Angel Yang</StyledSpan>
     </StyledFooter>
   )
 };
--- client/components/NavBar.js
+++ client/components/NavBar.js
@@ -6,27 +6,43 @@ const StyledNavBar = styled.nav`
   display: flex;
   flex-direction: row;
   align-items: center;
+  justify-content: space-between;
   height: 60px;
   padding: 10px;
+  padding-left: 30px;
   border-bottom: solid grey 1px;
 `;
 
-const StyledLink = styled(Link)`
-  position: absolute;
-  left: 40px;
+export const StyledLink = styled(Link)`
   display: flex;
   align-items: center;
   color: black;
+  font-size: 15px;
   text-decoration: none;
+`;
+
+export const LinkContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  height: 60px;
+  width: 150px;
+  margin-left: 20px;
+  margin-right: 20px;
+
 `;
 
 const NavBar = () => {
   return (
     <StyledNavBar>
-        <StyledLink to="/home">
-          <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSzh1ST7YGDBCT54ObrjXk_ORrRGwVL366Jb48Zz6r66YP2rcl2GNFRGQPg3hUvpOXHBEY&usqp=CAU' width="60" height="60"/>
-          HOME
-        </StyledLink>
+      <LinkContainer>
+        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSzh1ST7YGDBCT54ObrjXk_ORrRGwVL366Jb48Zz6r66YP2rcl2GNFRGQPg3hUvpOXHBEY&usqp=CAU' width="60" height="60"/>
+        <StyledLink to="/home">HOME</StyledLink>
+      </LinkContainer>
+      <LinkContainer>
+        <StyledLink to="/signup">Sign up</StyledLink>
+        <StyledLink to="/login">Log in</StyledLink>
+      </LinkContainer>
     </StyledNavBar>
   )
 };
