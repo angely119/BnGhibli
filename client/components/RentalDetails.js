a7b1d775bc81007f83f1fa2b08f3f9e0e19e0d65
Angel Yang <101146704+angely119@users.noreply.github.com>
2022-06-10 13:11:23 -0400

    RentalDetails styling

--- client/components/RentalDetails.js
+++ client/components/RentalDetails.js
@@ -24,6 +24,41 @@ const StyledRentalInfo = styled.span`
   font-weight: 500;
 `;
 
+const ImagesContainer = styled.div`
+  display: flex;
+  flex: row;
+  height: 450px;
+  border-radius: 15px;
+  margin-top: 20px;
+  overflow: hidden;
+  gap: 8px;
+`;
+//   border: solid grey 1px;
+
+const MainImage = styled.div`
+  background-image: url(${props => props.image});
+  display: flex;
+  width:100%;
+  height: 100%;
+  background-size: cover;
+  background-position: center;
+`;
+
+const SmallImagesContainer = styled.div`
+  display: flex;
+  width: 100%;
+  height: 100%;
+  flex-flow: row wrap;
+  gap: 8px 8px;
+`;
+
+const SmallImage = styled.div`
+  background-image: url(${props => props.image});
+  display: flex;
+  width: 49%;
+  background-size: cover;
+  background-position: center;
+`;
 
 // FUNCTIONAL COMPONENT
 const RentalDetails = (props) => {
@@ -31,9 +66,11 @@ const RentalDetails = (props) => {
   const rentalId = props.match.params.rentalId;
   const dispatch = useDispatch();
   const numOfReviews = rental.reviews && rental.reviews.length
-  // const avgRating = () => {
-  //   return rental.reviews.reduce(avg, cur)
-  // }
+  const avgRating = (reviews) => {
+    if (!reviews.length) return 'No reviews yet';
+    const avg = reviews.reduce((avgRating, curReview) => avgRating + curReview.rating / reviews.length, 0);
+    return `★${(Math.round(avg * 100) / 100).toFixed(2)}`;
+  };
 
   useEffect(() => {
     dispatch(fetchSingleRental(rentalId))
@@ -43,9 +80,19 @@ const RentalDetails = (props) => {
     <StyledRentalContainer>
       <StyledRentalName>{rental.rentalName}</StyledRentalName>
       <StyledRentalInfoContainer>
+        <StyledRentalInfo>{rental.reviews && avgRating(rental.reviews)}</StyledRentalInfo>
         <StyledRentalInfo>{`${numOfReviews ? numOfReviews : "No"} reviews`}</StyledRentalInfo>
         <StyledRentalInfo>{rental.location}</StyledRentalInfo>
       </StyledRentalInfoContainer>
+      <ImagesContainer>
+        <MainImage image={rental.imageUrls && rental.imageUrls[0]} />
+        <SmallImagesContainer>
+          <SmallImage image={rental.imageUrls && rental.imageUrls[1]} />
+          <SmallImage image={rental.imageUrls && rental.imageUrls[2]} />
+          <SmallImage image={rental.imageUrls && rental.imageUrls[3]} />
+          <SmallImage image={rental.imageUrls && rental.imageUrls[4]} />
+        </SmallImagesContainer>
+      </ImagesContainer>
     </StyledRentalContainer>
   )
 };
