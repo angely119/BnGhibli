021353b6e8b0e9f35fbf5170f24d7d233bc9eed9
Angel Yang <101146704+angely119@users.noreply.github.com>
2022-06-24 16:24:23 -0400

    public path

--- server/app.js
+++ server/app.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 const app = express();
 
 app.use(morgan('dev'));
-app.use(express.static(path.join(__dirname, '..', 'public')));
+app.use(express.static(path.join(__dirname, '..', '/public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
