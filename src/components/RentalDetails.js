01683270c2646cea3efe958427b9d09d1b2567c4
Angel Yang <101146704+angely119@users.noreply.github.com>
2022-07-31 01:15:54 -0400

    refactor: move RentalDetails styled components to styles

--- src/components/RentalDetails.js
+++ src/components/RentalDetails.js
@@ -3,157 +3,26 @@ import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { fetchSingleRental } from "../store/singleRental";
 import { RentalBookingForm } from "./index";
-import styled from "styled-components";
+import {
+  RentalContainer,
+  RentalHeaderName,
+  RentalHeaderContainer,
+  RentalHeaderInfo,
+  ImagesContainer,
+  MainImage,
+  SmallImagesContainer,
+  SmallImage,
+  RentalDetailsContainer,
+  RentalDetailsLeftContainer,
+  RentalDetailsHeader,
+  RentalDetailsHeaderLeft,
+  RentalDetailsTitle,
+  RentalDetailsSubtitle,
+  RentalDetailsHostIcon,
+  RentalDetailsHostBio,
+  RentalDetailsRightContainer
+} from "./styles";
 
-// STYLED COMPONENTS
-const RentalContainer = styled.div`
-  padding: 40px 80px
-`;
-
-const RentalHeaderName = styled.h1`
-  font-size: 24px;
-  margin-bottom: 5px;
-`;
-
-const RentalHeaderContainer = styled.div`
-  display: flex;
-  gap: 15px;
-`;
-
-const RentalHeaderInfo = styled.span`
-  font-size: 14px;
-  font-weight: 500;
-`;
-
-const ImagesContainer = styled.div`
-  display: flex;
-  flex: row;
-  height: 450px;
-  border-radius: 15px;
-  margin-top: 20px;
-  overflow: hidden;
-  gap: 10px;
-`;
-//   border: solid grey 1px;
-
-const MainImage = styled.div`
-  background-image: url(${props => props.image});
-  display: flex;
-  width: 100%;
-  height: 100%;
-  background-size: cover;
-  background-position: center;
-  -webkit-filter: brightness(100%);
-  &:hover {
-    -webkit-filter: brightness(70%);
-    -webkit-transition: all 1s ease;
-    -moz-transition: all 1s ease;
-    -o-transition: all 1s ease;
-    -ms-transition: all 1s ease;
-    transition: all 1s ease;
-  }
-`;
-
-const SmallImagesContainer = styled.div`
-  display: flex;
-  width: 100%;
-  height: 100%;
-  flex-flow: row wrap;
-  gap: 10px 10px;
-`;
-
-const SmallImage = styled.div`
-  background-image: url(${props => props.image});
-  display: flex;
-  width: 49%;
-  background-size: cover;
-  background-position: center;
-  -webkit-filter: brightness(100%);
-  &:hover {
-    -webkit-filter: brightness(70%);
-    -webkit-transition: all 1s ease;
-    -moz-transition: all 1s ease;
-    -o-transition: all 1s ease;
-    -ms-transition: all 1s ease;
-    transition: all 1s ease;
-  }
-`;
-
-const RentalDetailsContainer = styled.div`
-  display: flex;
-  height: 500px;
-`;
-
-const RentalDetailsLeftContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  width: 60%;
-  padding: 50px 0px;
-  padding-right: 15px;
-`;
-
-const RentalDetailsHeader = styled.div`
-  display: flex;
-  justify-content: space-between;
-  height: 80px;
-  padding-bottom: 20px;
-  border-bottom: solid #dad9d4 1px;
-  width: 100%;
-`;
-
-const RentalDetailsHeaderLeft = styled.div`
-  width: 558px;
-  height: 60px;
-`;
-
-const RentalDetailsTitle = styled.span`
-  font-size: 21px;
-  font-weight: 500;
-`;
-
-const RentalDetailsSubtitle = styled.div`
-  font-size: 15px;
-  font-weight: 400;
-  color: grey;
-`;
-
-const RentalDetailsHostIcon = styled.div`
-  width: 60px;
-  height: 60px;
-  background-image: url(${props => props.image});
-  background-size: cover;
-  background-position: center;
-  -moz-border-radius: 50px;
-  -webkit-border-radius: 50px;
-  border-radius: 50px;
-`;
-
-const RentalDetailsHostBio = styled.div`
-  height: auto;
-  width: 100%;
-  border-bottom: solid #dad9d4 1px;
-  padding: 32px 0px;
-  font-size: 16px;
-`;
-
-const RentalDetailsAmenities = styled.div`
-  height: 470px;
-  width: 100%;
-  border-bottom: solid #dad9d4 1px;
-  padding: 48px 0px;
-  font-size: 16px;
-`;
-
-const RentalDetailsRightContainer= styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: flex-end;
-  width: 40%;
-  padding: 50px 0px;
-`;
-
-// FUNCTIONAL COMPONENT
 const RentalDetails = (props) => {
   const rental = useSelector((state) => state.singleRental);
   const rentalId = props.match.params.rentalId;
